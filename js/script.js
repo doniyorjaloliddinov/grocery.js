39c86c4b166062f3df094ad2829f7a6d4d60436c
doniyorjonjaloliddinov <don2003.01.09@gmail.com>
2022-04-21 09:20:53 +0500

    add new project 'grocery store'

---
+++ js/script.js
@@ -0,0 +1,49 @@
+let elSearch = document.querySelector("#search");
+let elSearchBtn = document.querySelector(".search-btn");
+let elReadyList = document.querySelector(".ready-list");
+let elClearBtn = document.querySelector(".clear");
+let elCheckBtn = document.querySelector("#check-btn");
+let message = "Iltimos son kiritmang";
+
+let groceryLists = [];
+let marketsProducts = ["banana", "kiwi","lemon","peach"," pineaplle","apple","carrot","potato","tomato","cucumber","onio","raddish"];
+
+elSearchBtn.addEventListener("click", function(evt){
+  evt.preventDefault();
+
+  let search = elSearch.value.trim();
+  search = String(search);
+  if (search == "" || search == Number(search) ) {
+    elSearch.value = message;
+    elSearch.style.color = "red";
+    return
+  }
+    groceryLists.push(search);
+    console.log(groceryLists);
+
+  elSearch.style.color = "black";
+  // for (groceryList of groceryLists) {
+    let newLi = document.createElement("li");
+    newLi.textContent = groceryLists;
+    elReadyList.append(newLi);
+    newLi.style.color = "white";
+  // }
+})
+
+elClearBtn.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  elReadyList.innerHTML = "";
+})
+
+
+elCheckBtn.addEventListener("click", function(evt) {
+  evt.preventDefault();
+
+  if(search in marketsProducts) {
+    console.log(`${search} we have some kind products`);
+
+  }
+  else {
+    console.log("unfortunatly we don't have such kind of products");
+  }
+})
---
+++ python.py
@@ -0,0 +1,32 @@
+# grocery_list = []
+# user_input = input(str("Do'kondan sizgga nimalar kerak: "))
+# if user_input == int(user_input):
+#     print("Iltimos son kiritmang")
+# else:
+#     grocery_list.append(user_input)
+# print(grocery_list) 
+# savol = "Yaxshi ko\'rgan kitobingizni kiriting"
+# savol += "(Dasturni to\'xtatish uchun 'stop' deb yozing): "
+# shart = ''
+# while shart != 'stop':
+#     shart = input(savol)      
+#     if shart != 'stop':
+#         continue
+#     else:
+#         print("Dastur tugadi")
+
+    
+grocery_list = []
+user_input = str("Do'kondan sizgga nimalar kerak")
+user_input += "(Davom etasizmi ?(ha/yoq)): "
+shart = ''
+
+while shart != "yoq":
+    shart = input(user_input)
+    if shart == "ha":
+        grocery_list.append(user_input) 
+    else:
+        break
+print(f"Sizning olgan mahsulotlaringiz: {grocery_list}")
+        
+
